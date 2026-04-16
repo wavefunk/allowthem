de7879e430ed0e44c806a31ad6f9a027529d691d
wavefunk <developer@wavefunk.io>
2026-04-16 15:22:07 +0200

    feat: add AppState with FromRef impls for standalone server

---
+++ binaries/state.rs
@@ -0,0 +1,24 @@
+use std::sync::Arc;
+
+use axum::extract::FromRef;
+
+use allowthem_core::{AllowThem, AuthClient};
+
+#[derive(Clone)]
+pub struct AppState {
+    pub ath: AllowThem,
+    pub auth_client: Arc<dyn AuthClient>,
+    pub base_url: String,
+}
+
+impl FromRef<AppState> for Arc<dyn AuthClient> {
+    fn from_ref(state: &AppState) -> Self {
+        state.auth_client.clone()
+    }
+}
+
+impl FromRef<AppState> for AllowThem {
+    fn from_ref(state: &AppState) -> Self {
+        state.ath.clone()
+    }
+}
