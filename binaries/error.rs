b48cb6a87f976271fca57f60f26ba6934a79bd30
wavefunk <developer@wavefunk.io>
2026-04-16 16:24:46 +0200

    feat: add Auth variant to AppError

--- binaries/error.rs
+++ binaries/error.rs
@@ -4,6 +4,7 @@ use axum::response::{IntoResponse, Response};
 #[derive(Debug)]
 pub enum AppError {
     Template(minijinja::Error),
+    Auth(allowthem_core::AuthError),
 }
 
 impl From<minijinja::Error> for AppError {
@@ -12,11 +13,21 @@ impl From<minijinja::Error> for AppError {
     }
 }
 
+impl From<allowthem_core::AuthError> for AppError {
+    fn from(err: allowthem_core::AuthError) -> Self {
+        AppError::Auth(err)
+    }
+}
+
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
         match self {
             AppError::Template(e) => {
                 tracing::error!(error = %e, "template render failed");
+                StatusCode::INTERNAL_SERVER_ERROR.into_response()
+            }
+            AppError::Auth(e) => {
+                tracing::error!(error = %e, "auth error");
                 StatusCode::INTERNAL_SERVER_ERROR.into_response()
             }
         }
