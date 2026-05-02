8906dc4c2a3feca51986349af716355fbc456de9
wavefunk <developer@wavefunk.io>
2026-05-02 21:17:03 +0200

    feat: @allowthem/js index exports + align quickstart browser snippet

---
+++ packages/js/src/index.ts
@@ -0,0 +1,10 @@
+export { createAllowthemClient } from "./client.js";
+export { AuthError, ERROR_CODES } from "./errors.js";
+export type {
+  AllowthemClient,
+  ClientConfig,
+  LoginOptions,
+  LogoutOptions,
+  TokenResponse,
+  UserClaims,
+} from "./types.js";
